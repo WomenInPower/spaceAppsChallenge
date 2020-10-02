e0c8e230ec5392ff631e01020d0acab9b86204ea
Slowgen <kalibobailey@gmail.com>
2020-10-02 18:12:38 -0400

    Initial Commit: boilerplate

---
+++ secrets.js
@@ -0,0 +1,3 @@
+process.env.GOOGLE_CLIENT_ID = '1097897858957-iu62beo9qfjhlecc76uu4llr08ucqcr9.apps.googleusercontent.com'
+process.env.GOOGLE_CLIENT_SECRET = 'yO3Rj6wG91LDJYZXXwDj-X7E'
+process.env.GOOGLE_CALLBACK = '/auth/google/callback'
