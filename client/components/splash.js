fe701cd523b895fed0f8e9edf6a6a6a9af5bacf9
Slowgen <kalibobailey@gmail.com>
2020-10-03 15:01:18 -0400

    Feat: Splash page added, sign up page removed

---
+++ client/components/splash.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//splash page pre-login
+
+export const Splash = () => {
+
+  return (
+    <div>
+      <h1>Welcome to powersleep</h1>
+      <h2>Info about powersleep</h2>
+      <h3>The creators behind powersleep</h3>
+    </div>
+  )
+}
+
+export default Splash
