056b20c9838ceb509a011baf9e397e58d4cbc4c4
ceciliachung <ceciliayu.chung@gmail.com>
2020-10-03 16:49:06 -0400

    FEAT: google calendar API integration

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,41 @@
+const webpack = require('webpack')
+const dotenv = require('dotenv')
+
 const isDev = process.env.NODE_ENV === 'development'
+
+// Be able to use dotenv for Frontend env variables (e.g. secrets)
+// https://medium.com/@trekinbami/using-environment-variables-in-react-6b0a99d83cf5
+const env = dotenv.config().parsed
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next])
+  return prev
+}, {})
 
 module.exports = {
   mode: isDev ? 'development' : 'production',
   entry: [
     '@babel/polyfill', // enables async-await
-    './client/index.js'
+    './client/index.js',
   ],
   output: {
     path: __dirname,
-    filename: './public/bundle.js'
+    filename: './public/bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   devtool: 'source-map',
   watchOptions: {
-    ignored: /node_modules/
+    ignored: /node_modules/,
   },
   module: {
     rules: [
       {
         test: /\.jsx?$/,
         exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  }
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [new webpack.DefinePlugin(envKeys)],
 }
