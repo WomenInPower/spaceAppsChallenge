622ace4be943b6af168b96fd97e1fbd0d4dce7eb
Slowgen <kalibobailey@gmail.com>
2020-10-04 14:37:46 -0400

    Feat: calendar embedded in user-home

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -11,6 +11,7 @@ export const UserHome = props => {
   return (
     <div>
       <h3>Welcome, {email}</h3>
+      <iframe src="https://calendar.google.com/calendar/embed?height=600&amp;wkst=1&amp;bgcolor=%23050505&amp;ctz=UTC&amp;src=cG93ZXJzcGFjZWFwcHNAZ21haWwuY29t&amp;src=YWRkcmVzc2Jvb2sjY29udGFjdHNAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;src=ZW4udXNhI2hvbGlkYXlAZ3JvdXAudi5jYWxlbmRhci5nb29nbGUuY29t&amp;color=%23039BE5&amp;color=%2333B679&amp;color=%230B8043&amp;showCalendars=0&amp;title=Power%20Sleep" width="800" height="600"  scrolling="no" />
     </div>
   )
 }
