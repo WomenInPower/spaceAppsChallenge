5d7965ed5fd7038c2fbb347c2c27c09d8f99dbe7
Slowgen <kalibobailey@gmail.com>
2020-10-04 12:01:12 -0400

    Feat: Added styling to SPLASH page

--- client/components/splash.js
+++ client/components/splash.js
@@ -5,10 +5,16 @@ import React from 'react'
 export const Splash = () => {
 
   return (
-    <div>
-      <h1>Welcome to powersleep</h1>
-      <h2>Info about powersleep</h2>
-      <h3>The creators behind powersleep</h3>
+    <div id="splash">
+      <h1>welcome to power sleep</h1>
+      <div>
+        <h2>your body and mind know best</h2>
+        keeping a regular sleep schedule helps reset your circadian rhythm. Your body and mind are wise, we help you be in sync with them.
+      </div>
+      <div>
+        <h2>power sleep helps you</h2>
+        Find time-slots in between your mission critical tasks that you could use to take a well-deserved break and align your circadian rhythm.
+      </div>
     </div>
   )
 }
