c47f26becc8ca3abd01e5f9d39a0d164125add27
Lu Wang <loowang@hotmail.com>
2020-10-05 22:24:46 -0400

    update: improve algos

--- client/components/calendar-add.js
+++ client/components/calendar-add.js
@@ -1,140 +1,80 @@
-import React from 'react'
-import moment from 'moment-timezone'
+import React, {Component} from 'react'
+import ReactModal from 'react-modal'
+import {connect} from 'react-redux'
+import {addSleepEvent, generateSleepEvents, loadEvents} from '../store/calendar'
 
-// Google Calendar API
-const AddToCalendar = () => {
-  // Initializing all of the variables
-  const gapi = window.gapi
-  const CLIENT_ID = process.env.GOOGLE_CALENDAR_CLIENT_ID
-  const API_KEY = process.env.GOOGLE_CALENDAR_API_KEY
-  console.log('process.env keys: ' + Object.keys(process.env))
+export const gapi = window.gapi
+export const CLIENT_ID = process.env.GOOGLE_CALENDAR_CLIENT_ID
+export const API_KEY = process.env.GOOGLE_CALENDAR_API_KEY
+console.log('process.env keys: ' + Object.keys(process.env))
 
-  // Array of API discovery doc URLs for APIs used by the quickstart
-  const DISCOVERY_DOCS = [
-    'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
-  ]
-  // Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
-  const SCOPES = 'https://www.googleapis.com/auth/calendar.events'
+// Array of API discovery doc URLs for APIs used by the quickstart
+export const DISCOVERY_DOCS = [
+  'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
+]
+// Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
+export const SCOPES = 'https://www.googleapis.com/auth/calendar.events'
 
-  function sleepShift(events) {
-    let sleepEvent = {summary: 'Sleep', start: {}, end: {}}
-    events.forEach((event) => {
-      // if the event is flight on earth
-      if (event.summary === 'Flight Houston-Florida') {
-        let startDate = moment
-          .tz(event.start.dateTime, event.start.timeZone)
-          .date()
-        let endDate = moment.tz(event.end.dateTime, event.end.timeZone).date()
-        console.log('startDate:', startDate, 'endDate:', endDate)
-
-        let startTime = moment
-          .tz(event.start.dateTime, event.start.timeZone)
-          .hours()
-        let endTime = moment.tz(event.end.dateTime, event.end.timeZone).hours()
-        console.log('startTime:', startTime, 'endTime:', endTime)
-        // if the event is not overnight
-        if (startDate === endDate) {
-          let emptyBlock = 24 - (endTime - startTime + 4)
-          console.log('Empty Time Block:', emptyBlock)
-          let start
-          let end
-          let poss1 = Math.abs(0 - (startTime - 2))
-          let poss2 = Math.abs(23 - startTime)
-          console.log(
-            'Possible SleepHour 1:',
-            poss1,
-            'Possible SleepHour 2:',
-            poss2
-          )
-          const hourToStartSleep = poss1 < poss2 ? startTime : 0
-          console.log('Sleep at:', hourToStartSleep)
-          const hourToEndSleep = hourToStartSleep + 8
-          console.log('Wake up at:', hourToEndSleep)
-          // then convert the hour back to Google calendar timestamp format using moment.js
-          if (hourToStartSleep < 10) {
-            start =
-              event.start.dateTime.slice(0, 11) +
-              '0' +
-              hourToStartSleep +
-              event.start.dateTime.slice(13)
-          } else {
-            start =
-              event.start.dateTime.slice(0, 11) +
-              hourToStartSleep +
-              event.start.dateTime.slice(13)
-          }
-          if (hourToEndSleep < 10) {
-            end =
-              event.end.dateTime.slice(0, 11) +
-              '0' +
-              hourToEndSleep +
-              event.end.dateTime.slice(13)
-          } else {
-            end =
-              event.end.dateTime.slice(0, 11) +
-              hourToEndSleep +
-              event.end.dateTime.slice(13)
-          }
-          sleepEvent.start.dateTime = start
-          sleepEvent.end.dateTime = end
-        }
-      }
-    })
-    return sleepEvent
+class AddToCalendar extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showModal: false,
+    }
+    this.handleClick = this.handleClick.bind(this)
+    this.openModal = this.openModal.bind(this)
+  }
+  componentDidMount() {
+    loadEvents()
+    ReactModal.setAppElement('body')
   }
 
-  // function that handles whenever we click on a "add event to my calendar" type of button
-  const handleClick = () => {
-    gapi.load('client:auth2', async () => {
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES,
-      })
-
-      gapi.client.load('calendar', 'v3', () => {})
-
-      await gapi.auth2.getAuthInstance().signIn()
-
-      let sleepEvent
-      // get events
-      gapi.client.calendar.events
-        .list({
-          calendarId: 'primary',
-          timeMin: new Date().toISOString(),
-          showDeleted: false,
-          singleEvents: true,
-          maxResults: 10,
-          orderBy: 'startTime',
-        })
-        .then((response) => {
-          const events = response.result.items
-          sleepEvent = sleepShift(events)
-          console.log(sleepEvent)
-          console.log('EVENTS: ', events)
-        })
-
-      console.log(sleepEvent)
-      // Inserts the event (hard coded for now) to the authorized calendar
-      const request = gapi.client.calendar.events.insert({
-        calendarId: 'primary',
-        resource: sleepEvent,
-      })
-    })
+  openModal() {
+    this.setState({showModal: true})
+    this.props.generateSleepEvents(this.props.events)
   }
 
-  return (
-    <div>
-      <input
-        type="image"
-        src="images/sleep.png"
-        border="0"
-        alt="Submit"
-        onClick={handleClick}
-      />
-    </div>
-  )
+  handleClick(sleepEvent) {
+    this.props.addSleepEvent(sleepEvent)
+  }
+
+  render() {
+    const {events} = this.props
+    console.log(this.props)
+
+    return (
+      <div>
+        <input
+          type="image"
+          src="images/sleep.png"
+          border="0"
+          alt="Submit"
+          onClick={() => this.openModal()}
+        />
+
+        <ReactModal className="popup" isOpen={this.state.showModal}>
+          {events.map((sleepEvent, i) => (
+            <div key={i}>
+              From {sleepEvent.start.dateTime}
+              to {sleepEvent.end.dateTime}
+              <button
+                type="button"
+                onClick={(sleepEvent) => this.handleClick(sleepEvent)}
+              >
+                Add to Calendar
+              </button>
+            </div>
+          ))}
+          <button type="button" onClick={this.closeModal}>
+            Done
+          </button>
+        </ReactModal>
+      </div>
+    )
+  }
 }
 
-export default AddToCalendar
+const mapState = ({events}) => ({events})
+const mapDispatch = {generateSleepEvents, loadEvents, addSleepEvent}
+
+export default connect(mapState, mapDispatch)(AddToCalendar)
--- client/store/calendar.js
+++ client/store/calendar.js
@@ -1,9 +1,15 @@
-// import moment from 'moment-timezone'
-// import axios from 'axios'
-// import history from '../history'
+/* eslint-disable complexity */
+import moment from 'moment-timezone'
+import {
+  gapi,
+  CLIENT_ID,
+  API_KEY,
+  DISCOVERY_DOCS,
+  SCOPES,
+} from '../components/calendar-add'
 
 // Initializing all of the variables
-const gapi = window.gapi
+/* const gapi = window.gapi
 const CLIENT_ID = process.env.GOOGLE_CALENDAR_CLIENT_ID
 const API_KEY = process.env.GOOGLE_CALENDAR_API_KEY
 console.log('process.env keys: ' + Object.keys(process.env))
@@ -13,12 +19,13 @@ const DISCOVERY_DOCS = [
   'https://www.googleapis.com/discovery/v1/apis/calendar/v3/rest',
 ]
 // Authorization scopes required by the API; multiple scopes can be included, separated by spaces.
-const SCOPES = 'https://www.googleapis.com/auth/calendar.events'
+const SCOPES = 'https://www.googleapis.com/auth/calendar.events' */
 
 /**
  * ACTION TYPES
  */
 const GET_EVENTS = 'GET_EVENTS'
+const GET_SLEEP_EVENTS = 'GET_SLEEP_EVENTS'
 
 /**
  * INITIAL STATE
@@ -29,54 +36,47 @@ const defaultEvents = []
  * ACTION CREATORS
  */
 const getEvents = (events) => ({type: GET_EVENTS, events})
+const getSleepEvents = (events) => ({type: GET_SLEEP_EVENTS, events})
 
 /**
  * THUNK CREATORS
  */
-export const loadEvents = () => async (dispatch) => {
-  try {
-    gapi.load('client:auth2', async () => {
-      gapi.client.init({
-        apiKey: API_KEY,
-        clientId: CLIENT_ID,
-        discoveryDocs: DISCOVERY_DOCS,
-        scope: SCOPES,
-      })
-
-      gapi.client.load('calendar', 'v3', () => {})
-
-      await gapi.auth2.getAuthInstance().signIn()
-
-      let sleepEvent
-      // get all events of the calendar in the developer console
-      const response = await gapi.client.calendar.events.list({
-        calendarId: 'primary',
-        timeMin: new Date().toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 10,
-        orderBy: 'startTime',
-      })
-
-      const events = response.result.items
-      dispatch(getEvents(events || defaultEvents))
-      sleepEvent = sleepShift(events)
-      console.log('EVENTS: ', events)
-
-      console.log(sleepEvent)
-      // Inserts the event (hard coded for now) to the authorized calendar
-      const request = gapi.client.calendar.events.insert({
-        calendarId: 'primary',
-        resource: sleepEvent,
-      })
-      // Opens new tab with Google Calendar (might not need since we embedded)
-      request.execute((event) => {
-        window.open(event.htmlLink)
-      })
+export const loadEvents = () => (dispatch) => {
+  gapi.load('client:auth2', async () => {
+    gapi.client.init({
+      apiKey: API_KEY,
+      clientId: CLIENT_ID,
+      discoveryDocs: DISCOVERY_DOCS,
+      scope: SCOPES,
     })
-  } catch (err) {
-    console.error(err)
-  }
+    gapi.client.load('calendar', 'v3', () => {})
+
+    // get all events of the calendar in the developer console
+    const response = await gapi.client.calendar.events.list({
+      calendarId: 'primary',
+      timeMin: new Date().toISOString(),
+      showDeleted: false,
+      singleEvents: true,
+      maxResults: 10,
+      orderBy: 'startTime',
+    })
+    const events = response.result.items
+    console.log('EVENTS: ', events)
+    dispatch(getEvents(events || defaultEvents))
+  })
+}
+
+export const generateSleepEvents = (events) => (dispatch) => {
+  const sleepEvents = numbersToEvents(events)
+  dispatch(getSleepEvents(sleepEvents || defaultEvents))
+}
+
+export const addSleepEvent = (sleepEvent) => {
+  const request = gapi.client.calendar.events.insert({
+    calendarId: 'primary',
+    resource: sleepEvent,
+  })
+  request.execute()
 }
 
 /**
@@ -86,7 +86,104 @@ export default function (state = defaultEvents, action) {
   switch (action.type) {
     case GET_EVENTS:
       return action.events
+    case GET_SLEEP_EVENTS:
+      return action.events
     default:
       return state
   }
+}
+
+function sleepShift(events) {
+  let map = UTCToNumbers(events)
+  console.log(map)
+  let sleepEvents = {nap: [], fullCycle: []}
+  let fullCycleOptions = []
+  let napOptions = []
+
+  // iterate through the time array in the map
+  if (Math.abs(0 - map.time[0]) >= 10) fullCycleOptions.push(0, 8)
+
+  for (let i = 0; i < map.time.length; i++) {
+    // if times are the same and the respective dates aren't, they are overnight events
+    if (map.dates[i] !== map.dates[i + 1] && map.time[i] === map.time[i + 1]) {
+      // then add 24 hours to the end hour
+      map.time[i + 1] += 24
+    }
+    if (
+      map.time[i + 1] - map.time[i] <= 8 &&
+      map.time[i + 1] - map.time[i] > 0
+    ) {
+      napOptions.push(
+        map.time[i],
+        map.time[i] + (map.time[i + 1] - map.time[i])
+      )
+    } else if (map.time[i + 1] - map.time[i] >= 8) {
+      fullCycleOptions.push(map.time[i], Math.abs(24 - (map.time[i] + 8)))
+    }
+  }
+  sleepEvents.nap = napOptions
+  sleepEvents.fullCycle = fullCycleOptions
+  return sleepEvents
+}
+
+// covert all the timestamp to numbers for calculations
+function UTCToNumbers(events) {
+  let map = {dates: [], time: []}
+  events.forEach((event) => {
+    let startDate = moment.tz(event.start.dateTime, event.start.timeZone).date()
+    let endDate = moment.tz(event.end.dateTime, event.end.timeZone).date()
+    // console.log('startDate:', startDate, 'endDate:', endDate)
+
+    let startHour = moment
+      .tz(event.start.dateTime, event.start.timeZone)
+      .hours()
+    let endHour = moment.tz(event.end.dateTime, event.end.timeZone).hours()
+    // console.log('startHour:', startHour, 'endHour:', endHour)
+
+    map.dates.push(startDate, endDate)
+    map.time.push(startHour, endHour)
+  })
+
+  return map
+}
+
+// then convert the hour back to Google calendar timestamp, map timestamp to sleepEvent
+function numbersToEvents(events) {
+  let sleepOptions = []
+  let sleepEvents = sleepShift(events)
+  console.log('sleepEvents:', sleepEvents)
+
+  for (let i = 0; i < sleepEvents.nap.length; i++) {
+    let nap = {summary: 'nap', start: {}, end: {}}
+    nap.start.dateTime = moment().set('hour', sleepEvents.nap[i]).format()
+    if (sleepEvents.nap[i + 1]) {
+      nap.end.dateTime = moment()
+        .set('hour', sleepEvents.nap[i + 1])
+        .format()
+    }
+    if (i % 2) continue
+    sleepOptions.push(nap)
+  }
+  for (let i = 0; i < sleepEvents.fullCycle.length; i++) {
+    let sleep = {summary: 'sleep', start: {}, end: {}}
+    sleep.start.dateTime = moment()
+      .set('hour', sleepEvents.fullCycle[i])
+      .format()
+    if (
+      sleepEvents.fullCycle[i + 1] &&
+      sleepEvents.fullCycle[i + 1] < sleepEvents.fullCycle[i]
+    ) {
+      sleep.end.dateTime = moment()
+        .set('hour', sleepEvents.fullCycle[i + 1])
+        .add(1, 'd')
+        .format()
+    } else if (sleepEvents.fullCycle[i + 1]) {
+      sleep.end.dateTime = moment()
+        .set('hour', sleepEvents.fullCycle[i + 1])
+        .format()
+    }
+    if (i % 2) continue
+    sleepOptions.push(sleep)
+  }
+  return sleepOptions
 }
