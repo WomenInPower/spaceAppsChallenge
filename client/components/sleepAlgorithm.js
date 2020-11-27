644ee5f2c2016adcdf201c41c285a82ea83a9c89
Lu Wang <loowang@hotmail.com>
2020-11-26 23:19:18 -0500

    feature: simplified and restructured algorithm

--- client/components/sleepAlgorithm.js
+++ client/components/sleepAlgorithm.js
@@ -1,118 +1,62 @@
-import moment from 'moment-timezone'
+const moment = require('moment-timezone')
 
-export function sleepShift(events) {
-  let map = UTCToNumbers(events)
-  // console.log(map)
-  let sleepEvents = {nap: [], fullCycle: []}
-  let fullCycleOptions = []
-  let napOptions = []
+//Astronauts have to adjust their sleep schedule based on their mission schedule which can take a max of 3 days(72 hours), starting with flight from Houston to the launch site(either Russian or Florida), launching rocket and docking to ISS. Assuming after dock, there's one day to adjust their sleep schedule on ISS. Given a list of events, return an array of Google Calendar event (object) that contains possible time of a full 8-hour sleep cycle and nap time in a 48-hour period. They can also sleep on the plane. Use military time.
 
-  // iterate through the time array in the map
-  if (Math.abs(0 - map.time[0]) >= 10) fullCycleOptions.push(0, 8)
+// 48 hours timeline
+// houston to russia - 13 hours
+// houston to florida - 3 hours
+// earth to ISS - 24 hours
 
-  for (let i = 0; i < map.time.length; i++) {
-    // if times are the same and the respective dates aren't, they are overnight events
-    if (map.dates[i] !== map.dates[i + 1] && map.time[i] === map.time[i + 1]) {
-      // then add 24 hours to the end hour
-      map.time[i + 1] += 24
-    }
-    if (
-      map.time[i + 1] - map.time[i] <= 8 &&
-      map.time[i + 1] - map.time[i] > 0
-    ) {
-      napOptions.push(
-        map.time[i],
-        map.time[i] + (map.time[i + 1] - map.time[i])
-      )
-    } else if (map.time[i + 1] - map.time[i] >= 8) {
-      fullCycleOptions.push(map.time[i], Math.abs(24 - (map.time[i] + 8)))
+export default class SleepShiftSchedule {
+  constructor(events) {
+    this.events = events
+    this.eventSet = new Set()
+    this.unoccupied = new Map()
+    this.sleepOptions = []
+  }
+  // covert all the Google Calendar timestamp to numbers for calculations
+  utcToNumbers() {
+    this.events.forEach((event) => {
+      let startDate = moment
+        .tz(event.start.dateTime, event.start.timeZone)
+        .date()
+      let endDate = moment.tz(event.end.dateTime, event.end.timeZone).date()
+      // console.log('startDate:', startDate, 'endDate:', endDate)
+
+      let startHour = moment
+        .tz(event.start.dateTime, event.start.timeZone)
+        .hours()
+      let endHour = moment.tz(event.end.dateTime, event.end.timeZone).hours()
+      // console.log('startHour:', startHour, 'endHour:', endHour)
+      this.eventSet.add([startDate, endDate, startHour, endHour])
+    })
+  }
+
+  sleepShift() {
+    // to make matters simple, any occupied event won't be used for sleeping
+    let eventSet = Array.from(this.eventSet)
+    console.log(eventSet)
+    for (let i = eventSet.length - 1; i >= 0; i--) {
+      if (eventSet[i - 1] && eventSet[i][2] - eventSet[i - 1][3] >= 3) {
+        this.unoccupied.set(
+          eventSet[i - 1][3],
+          eventSet[i][2] - eventSet[i - 1][3]
+        )
+      }
     }
   }
-  sleepEvents.nap = napOptions
-  sleepEvents.fullCycle = fullCycleOptions
-  return sleepEvents
-}
 
-// covert all the timestamp to numbers for calculations
-export function UTCToNumbers(events) {
-  let map = {dates: [], time: []}
-  events.forEach((event) => {
-    let startDate = moment.tz(event.start, event.startTimeZone).date()
-    let endDate = moment.tz(event.end, event.endTimeZone).date()
-    // console.log('startDate:', startDate, 'endDate:', endDate)
-
-    let startHour = moment.tz(event.start, event.startTimeZone).hours()
-    let endHour = moment.tz(event.end, event.endTimeZone).hours()
-    // console.log('startHour:', startHour, 'endHour:', endHour)
-
-    map.dates.push(startDate, endDate)
-    map.time.push(startHour, endHour)
-  })
-
-  return map
-}
-
-// then convert the hour back to Google calendar timestamp, map timestamp to sleepEvent
-export function numbersToEvents(events) {
-  let sleepOptions = [
-    {
-      summary: 'nap',
-      start: {
-        dateTime: '2020-11-09T10:00:00-05:00',
-        timeZone: 'America/New_York',
-      },
-      end: {
-        dateTime: '2020-11-09T12:00:00-05:00',
-        timeZone: 'America/New_York',
-      },
-    },
-    {
-      summary: 'nap',
-      start: {
-        dateTime: '2020-11-09T14:00:00-05:00',
-        timeZone: 'America/New_York',
-      },
-      end: {
-        dateTime: '2020-11-09T16:00:00-05:00',
-        timeZone: 'America/New_York',
-      },
-    },
-  ]
-  let sleepEvents = sleepShift(events)
-  // console.log('sleepEvents:', sleepEvents)
-
-  for (let i = 0; i < sleepEvents.nap.length; i++) {
-    let nap = {summary: 'nap', start: {}, end: {}}
-    nap.start.dateTime = moment().set('hour', sleepEvents.nap[i]).format()
-    if (sleepEvents.nap[i + 1]) {
-      nap.end.dateTime = moment()
-        .set('hour', sleepEvents.nap[i + 1])
+  // then convert the hour back to Google calendar timestamp, map timestamp to sleepEvent Object that can be posted on Google Calendar
+  numbersToEvents() {
+    for (let [key, value] of this.unoccupied) {
+      let z = {summary: 'Zzzzz', start: {}, end: {}}
+      z.start.dateTime = moment().set('hour', key).format()
+      z.end.dateTime = moment()
+        .set('hour', key + value)
         .format()
+      this.sleepOptions.push(z)
     }
-    if (i % 2) continue
-    sleepOptions.push(nap)
+
+    return this.sleepOptions
   }
-  for (let i = 0; i < sleepEvents.fullCycle.length; i++) {
-    let sleep = {summary: 'sleep', start: {}, end: {}}
-    sleep.start.dateTime = moment()
-      .set('hour', sleepEvents.fullCycle[i])
-      .format()
-    if (
-      sleepEvents.fullCycle[i + 1] &&
-      sleepEvents.fullCycle[i + 1] < sleepEvents.fullCycle[i]
-    ) {
-      sleep.end.dateTime = moment()
-        .set('hour', sleepEvents.fullCycle[i + 1])
-        .add(1, 'd')
-        .format()
-    } else if (sleepEvents.fullCycle[i + 1]) {
-      sleep.end.dateTime = moment()
-        .set('hour', sleepEvents.fullCycle[i + 1])
-        .format()
-    }
-    if (i % 2) continue
-    sleepOptions.push(sleep)
-  }
-  // console.log('sleepOptions:', sleepOptions)
-  return sleepOptions.slice(0, 3)
 }
